ba3e77c0f71d36617e6c0feb5420c5ac92996b59
XuCai021130 <115910501+XuCai021130@users.noreply.github.com>
2023-03-25 21:13:56 -0400

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,18 @@
+''' hello.py is a simple webserver '''
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/hello")
+def hello_world():
+    return "<h1>Hello this wonderful World!</h1>"
+
+@app.route("/info")
+def intro_page():
+    return "<h1>Introduction</h1> <p>This is a webpage for practicing usign flask</p>"
+
+@app.route("/profile")
+def profile():
+    return "<p>name: Xu Cai </p><p>Date: 3, 18, 2023</p>"
+if __name__=='__main__':
+    app.run(debug=True,port=5001)
